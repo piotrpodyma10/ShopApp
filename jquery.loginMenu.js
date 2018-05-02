ab3f7c1b2ee93628bd4a0de0456095b52d3a3c72
Piotr Podyma <piotr.podyma10@gmail.com>
2018-05-02 17:31:56 +0200

    First commit

---
+++ jquery.loginMenu.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+    $("#signup_btn").click(function(){
+        $("#main").animate({left:"22.5%"},400); 
+        $("#main").animate({left:"30%"},500); 
+        $("#loginform").css("visibility","hidden");
+        $("#loginform").animate({left:"25%"},400);
+        
+        $("#signupform").animate({left:"75%"},400);
+        $("#signupform").animate({left:"30%"},500);
+        //$("#signupform").animate({visibility: "visible"});
+        //$("#signupform").css("visibility","visible");
+        $("#signupform").css({opacity: 0, visibility: "visible"}).animate({opacity: 1}, 200)
+    }); 
+    
+    $("#login_btn").click(function(){
+        $("#main").animate({left:"77.5%"},400); 
+        $("#main").animate({left:"70%"},500);
+        $("#signupform").css("visibility","hidden");
+        $("#signupform").animate({left:"75%"},400);
+        
+        $("#loginform").animate({left:"25%"},400);
+        $("#loginform").animate({left:"70%"},500);
+        $("#loginform").css("visibility","visible");
+    });
+});
+
+
+
+
+
+
+
+
